f616a965b123f50b625668abc9d25bbefd8fb4a0
Pratham Pancholi <pancholipratham33@gmail.com>
2022-02-20 21:43:06 +0530

    razor payment successfully implemented

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -20,3 +20,37 @@ exports.createRazorPayOrder = asyncHandler(async (req, res, next) => {
 
   res.status(200).json(order);
 });
+
+// payment success
+exports.paymentSuccess = asyncHandler(async (req, res, next) => {
+  const {
+    orderCreationId,
+    razorpayPaymentId,
+    razorpayOrderId,
+    razorpaySignature,
+  } = req.body;
+
+  const shasum = crypto.createHmac("sha256", "<YOUR RAZORPAY SECRET>");
+  shasum.update(`${orderCreationId}|${razorpayPaymentId}`);
+  const digest = shasum.digest("hex");
+
+  if (digest !== razorpaySignature)
+    return res.status(400).json({ msg: "Transaction not legit!" });
+
+  const newPayment = PaymentDetails({
+    razorpayDetails: {
+      orderId: razorpayOrderId,
+      paymentId: razorpayPaymentId,
+      signature: razorpaySignature,
+    },
+    success: true,
+  });
+
+  await newPayment.save();
+
+  res.json({
+    msg: "success",
+    orderId: razorpayOrderId,
+    paymentId: razorpayPaymentId,
+  });
+});
--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -3,3 +3,6 @@ const router = express.Router();
 const paymentController = require("../controllers/paymentController");
 
 router.post("/orders", paymentController.createRazorPayOrder);
+router.post("/success", paymentController.paymentSuccess);
+
+module.exports = router;
